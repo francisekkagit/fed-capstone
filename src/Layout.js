80042d4ccec59b6347889de1334657186673f7d7
francisekkagit <francisxekka@gmail.com>
2025-03-29 15:34:48 -0700

    Layout Setup + Navbar Complete + Logo Import

--- src/Layout.js
+++ src/Layout.js
@@ -5,13 +5,13 @@ import Footer from "./components/layout-specific/Footer";
 
 const Layout = () => {
   return (
-    <>
+    <div className='layout'>
       <Navbar/>
       <main>
         <Outlet/>
       </main>
       <Footer/>
-    </>
+    </div>
   )
 }
 
--- src/components/layout-specific/Navbar.js
+++ src/components/layout-specific/Navbar.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <picture>
+        <source srcSet='/LL_logo_m.png' className='logo-m' media='(max-width: 640px)'/>
+        <img src='/LL_Logo.jpg' alt='Little Lemon Logo'/>
+      </picture>
+      
+      <div className='navLinks'>
+        <a href='/'>Home</a>
+        <a href='/'>Menu</a>
+        <a href='/table-reservation'>Reservations</a>
+      </div>
+      <div className='navLinksMobile'>
+        <button onClick={(e) => setShowMobileMenu(!showMobileMenu)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d={showMobileMenu ? "M6 18 18 6M6 6l12 12" : "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"} />
+          </svg>
+        </button>
+        {showMobileMenu &&
+        <div className='navLinksMobileDD'>
+          <a href='/'>Home</a>
+          <a href='/'>Menu</a>
+          <a href='/table-reservation'>Reservations</a>
+        </div>}
+      </div>
+    </nav>
   )
 }
 
